019867f7467797eaca765548d818278668a266b3
Parimita Mohanty <pmohanty@unomaha.edu>
2019-06-28 16:30:44 -0500

    Initial commit

---
+++ vue-skills/src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>To Do List of {{ their_name }}, So many things to do!</h1>
+    <p>This is Parimita Mohanty’s To Do list developed in Vue.Js to learn Vue.Js.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      their_name: this.$route.params.name
+    }
+  }
+}
+</script>
